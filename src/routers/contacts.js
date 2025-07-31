5189fc8e33b19330e839dbf2fd904947c282dc63
Serhii Zhelobkov <meeelnik@gmail.com>
2025-07-31 21:02:50 +0300

    added some controllers, routers and handlers

---
+++ src/routers/contacts.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+// import { getAllContacts, getContactById } from './services/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import {
+  getAllContactsController,
+  getContactByIdController,
+  createContactController,
+} from '../controllers/contacts.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getAllContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,7 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
