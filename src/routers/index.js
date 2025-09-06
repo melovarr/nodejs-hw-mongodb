e002b5b9f49af4b8be7872bcbdd329e68d3d10b7
Serhii Zhelobkov <meeelnik@gmail.com>
2025-09-06 21:18:17 +0300

    added swagger docs

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import contactsRouter from './contacts.js';
 import authRouter from './auth.js';
+import { swaggerDocs } from '../middlewares/swaggerDocs.js';
 
 const router = Router();
 
 router.use('/contacts', contactsRouter);
 router.use('/auth', authRouter);
+
+router.use('/api-docs', swaggerDocs());
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
-// import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', 3005));
 
@@ -25,29 +24,10 @@ export const setupServer = () => {
       },
     }),
   );
-  // app.get('/', (req, res) => {
-  //   res.json({
-  //     message: 'Hello, world!',
-  //   });
-  // });
 
   app.use(router);
   app.use(notFoundHandler);
   app.use(errorHandler);
-  // app.use((req, res, next) => {
-  //   res.status(404).json({
-  //     status: '404',
-  //     message: 'Not Found',
-  //   });
-  // });
-
-  // app.use((err, req, res, next) => {
-  //   res.status(500).json({
-  //     status: '500',
-  //     message: 'Something went wrong!',
-  //     error: err.message,
-  //   });
-  // });
 
   app.use('/uploads', express.static(UPLOAD_DIR));
 
